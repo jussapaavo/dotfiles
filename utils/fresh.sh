2a6eaf8b2f7c6a9c82bbb32d7de0dd1fae4a8b59
Jussi Kämäräinen <jussi@jussikmr.fi>
2021-09-12 11:37:43 +0300

    update fresh.sh

--- utils/fresh.sh
+++ utils/fresh.sh
@@ -7,6 +7,9 @@ if test ! $(which omz); then
   /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
 fi
 
+# Install p10k theme for zsh
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -19,23 +22,14 @@ brew update
 brew tap homebrew/bundle
 brew bundle
 
-# Create a Sites directory
+# Create a dev directory
 mkdir $HOME/Development
 
-# Create sites subdirectories
+# Create dev subdirectories
 #mkdir $HOME/Development/blade-ui-kit
 
 # Clone Github repositories
-./clone.sh
-
-## <--- Replace with chezmoi
-# Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
-rm -rf $HOME/.zshrc
-ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
-
-# Symlink the Mackup config file to the home directory
-ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
-## ---> Replace with chezmoi
+./utils/clone.sh
 
 # Set macOS preferences - we will run this last because this will reload the shell
 source .macos
