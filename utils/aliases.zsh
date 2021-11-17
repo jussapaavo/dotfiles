ef31ab3ad72ab33f1fd5126395e44ed39f816193
Jussi Kämäräinen <jussi@jussikmr.fi>
2021-11-17 09:55:05 +0200

    add clean as an alias

--- utils/aliases.zsh
+++ utils/aliases.zsh
@@ -24,6 +24,7 @@ alias amend="git add . && git commit -a --amend --no-edit"
 alias force="git push --force-with-lease origin"
 alias nuke="git clean -df && git reset --hard"
 alias resolve="git add . && git commit --no-edit"
+alias clean='git branch --merged | egrep -v "(^\*|main|master|dev)" | xargs git branch -d'
 
 #alias gst="git status"
 #alias gb="git branch"
