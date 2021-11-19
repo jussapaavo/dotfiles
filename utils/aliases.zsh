f4c69fdabe4472cf64d93ac512bc6fa649a69418
Jussi Kämäräinen <jussi@jussikmr.fi>
2021-11-19 17:01:35 +0200

    update aliases

--- utils/aliases.zsh
+++ utils/aliases.zsh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/sh
 
 # Shortcuts
 alias reloadshell="source $HOME/.zshrc"
@@ -14,7 +14,6 @@ alias tf="terraform"
 alias jn="jupyter notebook"
 
 # Directories
-alias library="cd $HOME/Library"
 alias dev="cd $HOME/Development"
 alias devd="cd $HOME/Development/Dagmar"
 alias devp="cd $HOME/Development/Personal"
@@ -25,16 +24,3 @@ alias force="git push --force-with-lease origin"
 alias nuke="git clean -df && git reset --hard"
 alias resolve="git add . && git commit --no-edit"
 alias clean='git branch --merged | egrep -v "(^\*|main|master|dev)" | xargs git branch -d'
-
-#alias gst="git status"
-#alias gb="git branch"
-#alias gc="git checkout"
-#alias gl="git log --oneline --decorate --color"
-#alias commit="git add . && git commit -m"
-#alias diff="git diff"
-#alias pop="git stash pop"
-#alias pull="git pull"
-#alias push="git push"
-#alias stash="git stash -u"
-#alias unstage="git restore --staged ."
-#alias wip="commit wip"
