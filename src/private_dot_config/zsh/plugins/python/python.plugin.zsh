b890a42f17aa75b7794d26c65824c607568ab32c
Jussi Kämäräinen <jussi.kamarainen@iki.fi>
2023-10-13 17:47:03 +0300

    feat(cli): improve python plugin for zsh

--- src/private_dot_config/zsh/plugins/python/python.plugin.zsh
+++ src/private_dot_config/zsh/plugins/python/python.plugin.zsh
@@ -2,10 +2,23 @@
 #   - https://github.com/sorin-ionescu/prezto/blob/master/modules/python/init.zsh
 #   - https://github.com/mattmc3/zdotdir/blob/main/plugins/python/python.plugin.zsh
 
+# Load plugin functions.
+fpath=(${0:A:h}/functions $fpath)
+autoload -U $fpath[1]/*(.:t)
+
+# Setup config dirs
 export PYENV_ROOT="${XDG_DATA_HOME:-$HOME/.local/share}/pyenv"
 export PIPX_HOME="${XDG_DATA_HOME:-$HOME/.local/share}/pipx"
+export WORKON_HOME="${XDG_DATA_HOME:-$HOME/.local/share}/venvs"
+[[ -d "$WORKON_HOME" ]] || mkdir -p "$WORKON_HOME"
 
-# Pyenv
+# Init pyenv
 if command -v pyenv 1>/dev/null 2>&1; then
   eval "$(pyenv init - --no-rehash)"
 fi
+
+# Python aliases
+alias py2='python2'
+alias py3='python3'
+alias py='python3'
+alias pyva="source .venv/bin/activate"
