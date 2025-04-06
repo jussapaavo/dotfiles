439e960716d9d2b0d55fb11b437988ec10a7d915
Jussi Kämäräinen <jussi.kamarainen@iki.fi>
2025-04-06 11:42:46 +0300

    feat(python): migrate to uv managed python

--- src/dot_config/zsh/plugins/python/python.plugin.zsh
+++ src/dot_config/zsh/plugins/python/python.plugin.zsh
@@ -6,19 +6,7 @@
 fpath=(${0:A:h}/functions $fpath)
 autoload -U $fpath[1]/*(.:t)
 
-# Setup config dirs
-export PYENV_ROOT="${XDG_DATA_HOME:-$HOME/.local/share}/pyenv"
-export PIPX_HOME="${XDG_DATA_HOME:-$HOME/.local/share}/pipx"
-export WORKON_HOME="${XDG_DATA_HOME:-$HOME/.local/share}/venvs"
-[[ -d "$WORKON_HOME" ]] || mkdir -p "$WORKON_HOME"
-
-# Init pyenv
-if command -v pyenv 1>/dev/null 2>&1; then
-  eval "$(pyenv init - --no-rehash)"
-fi
-
 # Python aliases
-alias py2='python2'
-alias py3='python3'
 alias py='python3'
 alias pyva="source .venv/bin/activate"
+alias venv="source .venv/bin/activate"
