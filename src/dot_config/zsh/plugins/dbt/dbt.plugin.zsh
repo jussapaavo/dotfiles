3647b072b3384e3dbddbb9e7247b63a69f8e6130
Jussi Kämäräinen <jussi.kamarainen@iki.fi>
2026-03-13 22:59:29 +0200

    refactor(dbt): move dbt configs into plugin

---
+++ src/dot_config/zsh/plugins/dbt/dbt.plugin.zsh
@@ -0,0 +1,7 @@
+export DBT_PROFILES_DIR=$XDG_CONFIG_HOME/dbt
+
+# dbt aliases
+alias dbtf='~/.local/bin/dbt'
+alias dbtu='uv tool run --from dbt-core dbt'
+alias dbtr='dbt run'
+alias dbtrs='dbt run --select'
